c40c8935e8e6ee1b45ce0c48960e4aa981a5a639
Matt Jones <matt@starchup.com>
2020-10-29 17:42:35 -0400

    backward

--- sqlbackup.sh
+++ sqlbackup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #When passed a DB_SERVER, GS_BUCKET, and DB_NAME generate a backup of DB_NAME on DB_SERVER and save it to GS_BUCKET
 if  [ -z "$DB_NAME" ];then
-	gcloud sql export sql $DB_SERVER $GS_BUCKET/sqldump-$DB_SERVER-$DB_NAME-`date +"%m%d%y"`.sql --database=$DB_NAME --offload
+        gcloud sql export sql $DB_SERVER $GS_BUCKET/sqldump-$DB_SERVER-full-`date +"%m%d%y"`.sql  --offload
 else
-	gcloud sql export sql $DB_SERVER $GS_BUCKET/sqldump-$DB_SERVER-full-`date +"%m%d%y"`.sql  --offload
+        gcloud sql export sql $DB_SERVER $GS_BUCKET/sqldump-$DB_SERVER-$DB_NAME-`date +"%m%d%y"`.sql --database=$DB_NAME --offload
 fi
